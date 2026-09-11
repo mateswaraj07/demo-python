7d1e3a2199cd18b69e26eee12a2a29c54de3a240
mateswaraj07 <mateswaraj07@gmail.com>
2026-09-11 15:14:04 +0530

    Day 2 Commit 1

---
+++ day2_1.py
@@ -0,0 +1,6 @@
+# Write a program and determine whether the given age is adult/eligible to drive.
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to drive.")
+else:
+    print("You are not eligible to drive.")
---
+++ day2_2.py
@@ -0,0 +1,6 @@
+# program to find a number is even or odd
+n = int(input("Enter a number:"))
+if n%2==0:
+    print(n, "is a even number")
+else:
+    print(n, "is a odd number")
